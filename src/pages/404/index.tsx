1abfb2eae30d129eefbda8f229890fafb4609ada
Aliyan Rizwan <102222781+AliyanRizwanDev@users.noreply.github.com>
2026-05-09 06:06:39 +0200

    feat: update NotFound component layout and remove unnecessary text

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -4,14 +4,12 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="w-full flex flex-col items-center px-12 md:px-0 min-h-screen">
+    <div className="w-full flex flex-col items-center px-12 md:px-0 min-h-screen my-4">
+     
       <img
         className="h-80 w-80 md:h-[450px] md:w-[450px] 2xl:h-7/12 2xl:w-7/12"
         src="/images/404.jpg"
       />
-      <p className="text-black text-xl md:text-2xl font-bold mb-3">
-        404 — Page not found.
-      </p>
       <Link to={ROUTES.DASHBOARD} className="w-full sm:w-auto">
         <Button
           type="submit"
